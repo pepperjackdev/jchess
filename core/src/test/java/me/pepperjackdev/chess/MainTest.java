7884253d68dc35822bf56aa3ae093ce92971086b
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-12-25 15:31:56 +0100

    Created desktop module

---
+++ core/src/test/java/me/pepperjackdev/chess/MainTest.java
@@ -0,0 +1,12 @@
+package me.pepperjackdev.chess;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    void test() {
+        assertTrue(true); // rigorous test
+    }
+}
--- desktop/src/main/java/me/pepperjackdev/chess/App.java
+++ desktop/src/main/java/me/pepperjackdev/chess/App.java
@@ -1,6 +1,6 @@
 package me.pepperjackdev.chess;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
