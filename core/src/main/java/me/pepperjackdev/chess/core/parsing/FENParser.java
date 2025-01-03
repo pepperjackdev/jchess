5401727ad93d12aba46f091ac9d0923a348e89f8
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2025-01-03 15:16:58 +0100

    Updated FEN parser Methods to return the string representations of the provided regex and a method to parse the content of the first section (Piece Placement Data) were added.

--- core/src/main/java/me/pepperjackdev/chess/core/parsing/FENParser.java
+++ core/src/main/java/me/pepperjackdev/chess/core/parsing/FENParser.java
@@ -1,5 +1,10 @@
 package me.pepperjackdev.chess.core.parsing;
 
+import me.pepperjackdev.chess.core.piece.Piece;
+import me.pepperjackdev.chess.core.piece.PieceSide;
+import me.pepperjackdev.chess.core.piece.PieceType;
+import me.pepperjackdev.chess.core.position.MutablePosition;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,7 +38,83 @@ public class FENParser {
 
         // checking input FEN integrity
         if (!matcher.matches()) {
-            throw new IllegalArgumentException("Invalid FEN: " + fen);
+            throw new IllegalArgumentException("Malformed FEN: " + fen);
         }
+    }
+
+    public String getPiecePlacementDataString() {
+        return matcher.group("piecePlacement");
+    }
+
+    public String getActiveColorString() {
+        return matcher.group("activeColor");
+    }
+
+    public String getCastlingAvailabilityString() {
+        return matcher.group("castlingAvailability");
+    }
+
+    public String getEnPassantTargetSquareString() {
+        return matcher.group("enPassantTargetSquare");
+    }
+
+    public String getHalfmoveClockString() {
+        return matcher.group("halfmoveClock");
+    }
+
+    public String getFullmoveNumberString() {
+        return matcher.group("fullmoveNumber");
+    }
+
+    public Piece[][] getPiecePlacementData(int numberOfRanks, int numberOfFiles) {
+        MutablePosition position = new MutablePosition(numberOfRanks - 1, 0);
+        Piece[][] piecePlacementData = new Piece[numberOfRanks][numberOfFiles];
+
+        for (String rank: getPiecePlacementDataString().split("/")) {
+            for (char placementSymbol: rank.toCharArray()) {
+                if (placementSymbol >= '1' && placementSymbol <= '9') {
+                    // the placement symbol represents a white space over the chessboard
+                    int filesToSkip = placementSymbol - '0';
+                    System.out.println("Skipping file " + filesToSkip);
+                    position.moveByFiles(filesToSkip);
+                    continue;
+                }
+
+                Piece toPlace = new Piece(
+                        getPieceType(placementSymbol),
+                        getPieceSide(placementSymbol)
+                );
+
+                System.out.printf("Placing %s at %d-%d\n", toPlace, position.rank(), position.file());
+                piecePlacementData[position.rank()][position.file()] = toPlace;
+
+                position.moveToNextFile();
+            }
+
+            position.setFile(0);
+
+            if (position.rank() > 0) {
+                position.moveToPrevRank();
+            }
+        }
+
+        return piecePlacementData;
+    }
+
+    private PieceType getPieceType(char piecePlacementSymbol) {
+        return switch (Character.toLowerCase(piecePlacementSymbol)) {
+            case 'p' -> PieceType.PAWN;
+            case 'n' -> PieceType.KNIGHT;
+            case 'b' -> PieceType.BISHOP;
+            case 'r' -> PieceType.ROOK;
+            case 'q' -> PieceType.QUEEN;
+            case 'k' -> PieceType.KING;
+            default ->
+                    throw new IllegalStateException("Unexpected value: " + Character.toLowerCase(piecePlacementSymbol));
+        };
+    }
+
+    private PieceSide getPieceSide(char piecePlacementSymbol) {
+        return (Character.isUpperCase(piecePlacementSymbol)) ? PieceSide.WHITE : PieceSide.BLACK;
     }
 }
--- core/src/test/java/me/pepperjackdev/chess/core/parsing/FENParserTest.java
+++ core/src/test/java/me/pepperjackdev/chess/core/parsing/FENParserTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FENParserTest {
+
+    private static final String TEST_FEN_STRING = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
     private FENParser underTest;
 
     @BeforeEach
     public void setUp() {
-        this.underTest = new FENParser("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        this.underTest = new FENParser(TEST_FEN_STRING);
     }
 
     @Test
@@ -23,5 +26,53 @@ public class FENParserTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new FENParser("hello/world/this/is/not/a/fen/string w KQkq - 0 1");
         });
+    }
+
+    @Test
+    public void testGetPiecePlacementString() {
+        assertEquals(
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+                underTest.getPiecePlacementDataString()
+        );
+    }
+
+    @Test
+    public void testActiveColorString() {
+        assertEquals(
+                "w",
+                underTest.getActiveColorString()
+        );
+    }
+
+    @Test
+    public void testGetCastlingAvailabilityString() {
+        assertEquals(
+                "KQkq",
+                underTest.getCastlingAvailabilityString()
+        );
+    }
+
+    @Test
+    public void testGetEnPassantTargetSquareString() {
+        assertEquals(
+                "-",
+                underTest.getEnPassantTargetSquareString()
+        );
+     }
+
+    @Test
+    public void testGetHalfmoveClockString() {
+        assertEquals(
+                "0",
+                underTest.getHalfmoveClockString()
+        );
+    }
+
+    @Test
+    public void testGetFullmoveNumberString() {
+        assertEquals(
+                "1",
+                underTest.getFullmoveNumberString()
+        );
     }
 }
